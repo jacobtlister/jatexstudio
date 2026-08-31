2909caa691e782896f0047e0f5581f38abba1504
jacobtlister <jacoblister@protonmail.com>
2026-09-01 09:54:55 +1200

    renamed openinbrowser.sh and updated description and comments

--- scripts/open-dir-in-dolphin.sh
+++ scripts/open-dir-in-dolphin.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# File: openinbrowser.sh
+# File: open-dir-in-dolphin.sh
 
 : <<'info'
     required packages
         dolphin
+        kdotool
 
     description
-        opens dolphin (orphaned) at the directory of the file being currently edited
+        opens, orphans, and focuses on dolphin at the directory stored in the input argument
 info
 
+# open and orphan dolphin at the directory $1
 ((dolphin "${1}" &> /dev/null;) &)
 
 # search for instances of windows with containing dolphin in it's title and save the first
@@ -17,4 +19,5 @@ windowsearch=$(kdotool search --name --class --limit 1 "dolphin")
 # if there are no windows found, exit silently
 [[ -z "${windowsearch}" ]] && exit 0
 
+# focus on dolphin window
 kdotool windowactivate "${windowsearch}"
